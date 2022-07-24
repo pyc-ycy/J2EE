6405b53f02b3dcc7e6d4447d4934af9815b67f9a
pyc <2923616405@qq.com>
2022-07-24 08:37:34 +0800

    Add files

--- src/main/java/base/util/ArrayListLearn.java
+++ src/main/java/base/util/ArrayListLearn.java
@@ -1,5 +1,27 @@
 package base.util;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ArrayListLearn {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr1 = new ArrayList<Integer>();
+        ArrayList<Character> arr2 = new ArrayList<>(10);
+        arr1.add(14);
+        arr1.add(32);
+        System.out.println("The element in arr1:");
+        for (Integer e : arr1){
+            System.out.print(e+",");
+        }
+        System.out.print(" length: " + arr1.size() + "\n");
+        Character[] c = {'a', 'b', 'c', 'd', 'e'};
+        arr2.addAll(Arrays.stream(c).toList());
+        System.out.println("The element in arr2:");
+        for (Character e : arr2){
+            System.out.print(e+",");
+        }
+        System.out.print(" length: " + arr2.size());
+
+    }
 
 }
